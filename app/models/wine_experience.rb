4d3c174ac90910522d81f53e4feb24336d726e42
Natalia Gatti <gattinati.ng@gmail.com>
2021-01-23 17:18:05 -0300

    Added model Booking

--- app/models/wine_experience.rb
+++ app/models/wine_experience.rb
@@ -2,7 +2,7 @@ class WineExperience < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :booking
-  validates :title, :description, :date, :price  presence :true
+  validates :title, :description, :date, :price,  presence: true
   validates :price, numericality: { only_integer: true }
-  validates :date, format: { with: /\d{2}\/\d{2}\/\d{4}/, message: "Date must be in the following format: mm/dd/yyyy" }
+  # validates :date, format: { with: /\d{2}\/\d{2}\/\d{4}/, message: "Date must be in the following format: mm/dd/yyyy" }
 end
