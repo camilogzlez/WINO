126f80f4225ac86f9cf7a7b78e3e7028e51723ac
Natalia Gatti <gattinati.ng@gmail.com>
2021-01-26 19:36:26 -0300

    Change validation date in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :user
+  belongs_to :client, foreign_key: "user_id", class_name: "User"
   belongs_to :wine_experience
 
 end
--- app/models/wine_experience.rb
+++ app/models/wine_experience.rb
@@ -1,8 +1,16 @@
 class WineExperience < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, foreign_key: "user_id", class_name: "User"
+  # belongs_to :user
   has_many :bookings
-  has_many :users, through: :booking
+  # has_many :users, through: :booking
+
   validates :title, :description, :date, :price,  presence: true
   validates :price, numericality: { only_integer: true }
-  # validates :date, format: { with: /\d{2}\/\d{2}\/\d{4}/, message: "Date must be in the following format: mm/dd/yyyy" }
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.current
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
