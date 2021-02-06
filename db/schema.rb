376d83f07f90aead4b3a4fcec50ea259dfaec0d1
camilo gonzalez <camilogzlez@gmail.com>
2021-02-06 15:22:08 -0300

    schema added

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_232833) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "owner", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
